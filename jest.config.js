c554405f67cde28ad100f7ec63b5502bf958d2c5
Erik Sutherland <erik@mercury.com>
2024-11-15 16:09:58 -0800

    only run tests from src

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  rootDir: 'src',
   preset: 'ts-jest',
   testEnvironment: 'node', // Or 'jsdom' if you need a browser-like environment
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
